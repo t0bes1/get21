e9f4e1cfa0351b8c49f4809062264cd149c0b739
t0bes1 <131770004+t0bes1@users.noreply.github.com>
2023-07-19 11:43:56 +0000

    Add card library to core game structure

--- script.js
+++ script.js
@@ -13,48 +13,91 @@ standButton.addEventListener("click", function () {
     houseCards();
 });
 
-
 let hitButton = document.getElementById('hit-button');
 hitButton.addEventListener("click", function () {
     hitCard();
 });
+
+let resetButton = document.getElementById('reset-button');
+resetButton.addEventListener("click", function () {
+    resetGame();
+});
+
+const cards = [
+    { 'rank': 'Ace of hearts', 'value': 11 },
+    { 'rank': 'Two of hearts', 'value': 2 },
+    { 'rank': 'Three of hearts', 'value': 3 },
+    { 'rank': 'Four of hearts', 'value': 4 },
+    { 'rank': 'Five of hearts', 'value': 5 },
+    { 'rank': 'Six of hearts', 'value': 6 },
+    { 'rank': 'Seven of hearts', 'value': 7 },
+    { 'rank': 'Eight of hearts', 'value': 8 },
+    { 'rank': 'Nine of hearts', 'value': 9 },
+    { 'rank': 'Ten of hearts', 'value': 10 },
+    { 'rank': 'Jack of hearts', 'value': 10 },
+    { 'rank': 'Queen of hearts', 'value': 10 },
+    { 'rank': 'King of hearts', 'value': 10 },
+    { 'rank': 'Ace of clubs', 'value': 11 },
+    { 'rank': 'Two of clubs', 'value': 2 },
+    { 'rank': 'Three of clubs', 'value': 3 },
+    { 'rank': 'Four of clubs', 'value': 4 },
+    { 'rank': 'Five of clubs', 'value': 5 },
+    { 'rank': 'Six of clubs', 'value': 6 },
+    { 'rank': 'Seven of clubs', 'value': 7 },
+    { 'rank': 'Eight of clubs', 'value': 8 },
+    { 'rank': 'Nine of clubs', 'value': 9 },
+    { 'rank': 'Ten of clubs', 'value': 10 },
+    { 'rank': 'Jack of clubs', 'value': 10 },
+    { 'rank': 'Queen of clubs', 'value': 10 },
+    { 'rank': 'King of clubs', 'value': 10 },
+    { 'rank': 'Ace of diamonds', 'value': 11 },
+    { 'rank': 'Two of diamonds', 'value': 2 },
+    { 'rank': 'Three of diamonds', 'value': 3 },
+    { 'rank': 'Four of diamonds', 'value': 4 },
+    { 'rank': 'Five of diamonds', 'value': 5 },
+    { 'rank': 'Six of diamonds', 'value': 6 },
+    { 'rank': 'Seven of diamonds', 'value': 7 },
+    { 'rank': 'Eight of diamonds', 'value': 8 },
+    { 'rank': 'Nine of diamonds', 'value': 9 },
+    { 'rank': 'Ten of diamonds', 'value': 10 },
+    { 'rank': 'Jack of diamonds', 'value': 10 },
+    { 'rank': 'Queen of diamonds', 'value': 10 },
+    { 'rank': 'King of diamonds', 'value': 10 },
+    { 'rank': 'Ace of spades', 'value': 11 },
+    { 'rank': 'Two of spades', 'value': 2 },
+    { 'rank': 'Three of spades', 'value': 3 },
+    { 'rank': 'Four of spades', 'value': 4 },
+    { 'rank': 'Five of spades', 'value': 5 },
+    { 'rank': 'Six of spades', 'value': 6 },
+    { 'rank': 'Seven of spades', 'value': 7 },
+    { 'rank': 'Eight of spades', 'value': 8 },
+    { 'rank': 'Nine of spades', 'value': 9 },
+    { 'rank': 'Ten of spades', 'value': 10 },
+    { 'rank': 'Jack of spades', 'value': 10 },
+    { 'rank': 'Queen of spades', 'value': 10 },
+    { 'rank': 'King of spades', 'value': 10 },
+];
 
 /**
  * player cards
  */
 
 function playerCards() {
-    let num1 = Math.floor(Math.random() * 10) + 1;
-    let num2 = Math.floor(Math.random() * 10) + 1;
-
-    console.log(num1);
-    console.log(num2);
+    let num1 = Math.floor(Math.random() * 52) + 1;
+    let num2 = Math.floor(Math.random() * 52) + 1;
 
     displayPlayerCards(num1, num2);
-    switchGameButtons();
-}
-
-function displayPlayerCards(operand1, operand2) {
-
-    document.getElementById('player-card-1').textContent = operand1;
-    document.getElementById('player-card-2').textContent = operand2;
-    calculatePlayerTotal();
-}
-
-function calculatePlayerTotal() {
-    let card1 = parseInt(document.getElementById('player-card-1').innerText);
-    let card2 = parseInt(document.getElementById('player-card-2').innerText);
-    let card3 = parseInt(document.getElementById('player-card-3').innerText);
-    let playerScore = card1 + card2 + card3;
-    document.getElementById('player-score').textContent = playerScore;
-    checkPlayerTotal(playerScore);
-}
-
-function switchGameButtons() {
-
     dealButton.style.visibility = "hidden";
     hitButton.style.visibility = "visible";
     standButton.style.visibility = "visible";
+}
+
+function displayPlayerCards(operand1, operand2) {
+    let playerCard1 = cards[operand1 - 1];
+    let playerCard2 = cards[operand2 - 1];
+    document.getElementById('player-card-1').textContent = playerCard1.rank;
+    document.getElementById('player-card-2').textContent = playerCard2.rank;
+    document.getElementById('player-score').textContent = playerCard1.value + playerCard2.value;
 }
 
 function houseCards() {
@@ -67,49 +110,81 @@ function houseCards() {
 }
 
 function displayHouseCards(operand1, operand2) {
-
-    document.getElementById('house-card-1').textContent = operand1;
-    document.getElementById('house-card-2').textContent = operand2;
-    calculateHouseTotal();
-}
-
-function calculateHouseTotal() {
-    let card4 = parseInt(document.getElementById('house-card-1').innerText);
-    let card5 = parseInt(document.getElementById('house-card-2').innerText);
-    let houseScore = card4 + card5;
-    console.log(houseScore);
+    let houseCard1 = cards[operand1 - 1];
+    let houseCard2 = cards[operand2 - 1];
+    let houseScore = houseCard1.value + houseCard2.value;
+    document.getElementById('house-card-1').textContent = houseCard1.rank;
+    document.getElementById('house-card-2').textContent = houseCard2.rank;
     document.getElementById('house-score').textContent = houseScore;
     checkGameResult(houseScore);
 }
 
 function hitCard() {
-    let num3 = Math.floor(Math.random() * 20) + 1;
-    document.getElementById('player-card-3').textContent = num3;
+    let playercard3 = cards[Math.floor(Math.random() * 52)];
+    document.getElementById('player-card-3').textContent = playercard3.rank;
     hitButton.style.visibility = "hidden";
     standButton.style.visibility = "hidden";
-    calculatePlayerTotal();
+    calculatePlayerTotal(playercard3);
 }
 
-function checkPlayerTotal(playerScore) {
-    console.log(playerScore);
-    if (playerScore > 21) {
-        document.write('<h1>Bust!</h1>');
-        document.write('<input type="button" value="Reload Page" onClick="window.location.reload(true)">');
+function calculatePlayerTotal(playercard3) {
+    let finalPlayerScore = parseInt(document.getElementById('player-score').textContent) + playercard3.value;
+    console.log(finalPlayerScore);
+    document.getElementById('player-score').textContent = finalPlayerScore;
+    checkPlayerTotal(finalPlayerScore);
+}
+
+function checkPlayerTotal(finalPlayerScore) {
+    if (finalPlayerScore > 21) {
+        document.getElementById('result-box').textContent = "Bust! You Lose!";
+        resetButton.style.visibility = "visible";
+        reduceScore();
     } else {
-        console.log("live");
-        standButton.style.visibility = "visible";
+        houseCards();
     }
 }
 
 function checkGameResult(houseScore) {
     let playerResult = parseInt(document.getElementById('player-score').innerText);
     if (playerResult > houseScore) {
-        document.write('<h1>Win!</h1>');
-        document.write('<input type="button" value="Reload Page" onClick="window.location.reload(true)">');
+        document.getElementById('result-box').textContent = "Win";
+        resetButton.style.visibility = "visible";
+        growScore();
     }
     else {
-        document.write('<h1>Lose!</h1>');
-        document.write('<input type="button" value="Reload Page" onClick="window.location.reload(true)">');
+        document.getElementById('result-box').textContent = "Lose";
+        resetButton.style.visibility = "visible";
+        reduceScore();
     }
+}
 
+/**
+ * increment scores up (grow) or down (reduce) depending on win or lose/bust
+ */
+
+function growScore() {
+    let oldScore = parseInt(document.getElementById("score-box").innerText);
+    document.getElementById("score-box").innerText = ++oldScore;
+}
+
+function reduceScore() {
+    let oldScore = parseInt(document.getElementById("score-box").innerText);
+    document.getElementById("score-box").innerText = oldScore - 1;
+}
+
+/**
+ * return game to initial state for user to play again
+ */
+
+function resetGame() {
+    dealButton.style.visibility = "visible";
+    resetButton.style.visibility = "hidden";
+    document.getElementById('house-card-1').textContent = "";
+    document.getElementById('house-card-2').textContent = "";
+    document.getElementById('house-score').textContent = 0;
+    document.getElementById('player-card-1').textContent = "";
+    document.getElementById('player-card-2').textContent = "";
+    document.getElementById('player-card-3').textContent = "";
+    document.getElementById('player-score').textContent = 0;
+    document.getElementById('result-box').textContent = "";
 }
